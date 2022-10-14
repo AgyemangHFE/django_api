8b7e05aedab7dce485bedb5058cfc5e40104269c
Bofah <bofahchristopher@gmail.com>
2022-10-14 13:19:20 +0000

    testing api

--- djangonative/tests.py
+++ djangonative/tests.py
@@ -4,29 +4,51 @@ from django.urls import reverse,resolve
 from .import views
 from rest_framework.test import APITestCase
 from rest_framework import status
+from djangonative.urls import path
 
 # Create your tests here.
-class APITesting(APITestCase):
-    def test_api_list(self):
-        response=self.client.get(reverse('api-overview'))
+class TestUrls(SimpleTestCase):
+    def test_api_overview(self):
         
-        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        url=reverse('api-overview')
+        
+        self.assertEquals(resolve(url).func,views.apiOverview)
+    
+    def test_api_task_list(self):
+       
+        url=reverse('task-list')
+        
+        self.assertEquals(resolve(url).func,views.tasklist)
 
-class APITesting(APITestCase):
-    def test_api_list(self):
-        response=self.client.get(reverse('task-list'))
+    def test_api_order_list(self):
         
-        self.assertEqual(response.status_code,status.HTTP_200_OK)
- 
-class APITesting(APITestCase):
-    def test_api_list(self):
-        response=self.client.get(reverse('dispatch-list'))
+        url=reverse('order-list')
         
-        self.assertEqual(response.status_code,status.HTTP_200_OK)
-        
-class APITesting(APITestCase):
-    def test_api_list(self):
-        response=self.client.get(reverse('person-list'))
-        
-        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEquals(resolve(url).func,views.orders)
 
+       
+
+    
+    def test_api_restaurant_list(self):
+        
+        url=reverse('restaurant-list')
+        
+        self.assertEquals(resolve(url).func,views.restaurantlist)
+    
+    def test_api_register_person(self):
+        
+        url=reverse('register-person')
+        
+        self.assertEquals(resolve(url).func,views.registerperson)
+    
+    def test_api_create_task(self):
+        
+        url=reverse('task-create')
+        
+        self.assertEquals(resolve(url).func,views.taskCreate)
+
+    def test_api_delete_task(self):
+        
+        url=reverse('person-list')
+        
+        self.assertEquals(resolve(url).func,views.personlist)
